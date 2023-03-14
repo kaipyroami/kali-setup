9d7bfdf3779fa5ba585cfad188b750ee73e6c60f
Kyle Crockett <kyle@kyle-crockett.com>
2023-03-14 16:33:25 -0700

    Initial setup script

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update system
+echo "Updating system packages..."
+sudo apt-get update && sudo apt-get upgrade -y
+
+# Install basic packages
+echo "Installing basic packages..."
+sudo apt-get install -y nano git curl wget htop
+
+# Set up user
+read -p "Enter your desired password: " PASSWORD
+sudo passwd $PASSWORD
+
+# Install additional packages (optional)
+echo "Installing additional packages..."
+sudo apt-get install -y build-essential python3 python3-pip
+
+# Configure timezone
+echo "Configuring timezone..."
+sudo dpkg-reconfigure tzdata
+
+# Add user-specific configurations (optional)
+echo "Setting up user configurations..."
+sudo -u kali bash -c "git clone https://github.com/kaipyroami/dotfiles.git ~/dotfiles"
+sudo -u kali bash -c "cd ~/dotfiles && ./install.sh"
+
+# Reboot system
+echo "Configuration complete. Rebooting system..."
+sudo reboot
